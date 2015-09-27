36823491e4e9a1bada599d9b6d441b29b70ba8da
samisdat <bastian.sackermann@gmail.com>
2015-09-27 21:36:47 +0000

    opening an remote image

---
+++ schleuder_modules/default_actions/open.js
@@ -0,0 +1,103 @@
+var lwip = require('lwip');
+var q = require('q');
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+require('buffer');
+
+
+var fileType = require('file-type');
+
+var openLocal = function(filename){
+	var deferred = q.defer();
+
+	fs.readFile(filename, function(error, buffer){
+		if(error){
+			deferred.reject();
+		}
+
+  		lwip.open(buffer, 'jpg', function(err, image){
+  			deferred.resolve(image);	
+		});
+	});
+
+	return deferred.promise;
+};
+
+var getRequestOptions = function(imageUrl){
+	var options = {
+	  hostname: undefined,
+	  port: undefined,
+	  path: undefined,
+	  method: 'GET'
+	};
+
+	var urlParsed = url.parse(imageUrl);
+
+	if(urlParsed.protocol === 'http:'){
+		options.port = 80;
+	}
+	else if(urlParsed.protocol === 'https:'){
+		options.port = 443;
+	}
+
+	options.hostname = urlParsed.hostname; 
+	options.path = urlParsed.path; 
+
+	return options;
+
+};
+
+var openRemote = function(imageUrl){
+
+	var deferred = q.defer();	
+
+	var options = getRequestOptions(imageUrl);
+
+	var req = http.request(options, function(res) {
+
+		var data = [];
+
+		res.on('data', function (chunk) {
+
+			data.push(chunk)
+
+		});
+
+		res.on('end', function() {
+
+			var buffer = Buffer.concat(data);
+			
+			var type = fileType(buffer);
+			var format = false;
+
+			if('image/png' === type.mime){
+				format = 'png';
+			}
+			else if('image/jpeg' === type.mime){
+				format = 'jpg';				
+			}
+
+			if(false === format){
+				deferred.reject();
+			}
+
+			lwip.open(buffer, format, function(error, image){
+				console.log(image.width())
+				deferred.resolve(image);	
+			});
+
+		});
+	});
+
+	req.on('error', function(e) {
+	  console.log('problem with request: ' + e.message);
+	});
+
+	req.end();
+
+	return deferred.promise;	
+};
+
+
+module.exports = openRemote;
