810285ea42aed6dd5043270831876d713545fdcf
samisdat <bastian.sackermann@gmail.com>
2015-09-27 20:06:37 +0000

    rudimentari working with remote url and buffer

--- routes/index.js
+++ routes/index.js
@@ -1,81 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
-var fs = require('fs');
 var lwip = require('lwip');
 
-var open = require('../actions/open');
-var scale = require('../actions/scale');
+var open = require('../schleuder_modules/actions/open');
+var resize = require('../schleuder_modules/actions/resize');
+var scale = require('../schleuder_modules/actions/scale');
+var rotate = require('../schleuder_modules/actions/flip');
+var send = require('../schleuder_modules/actions/send');
 
-/* GET home page. */
+router.get('/local-image', function(req, res, next) {
+  
+  var filename = '/var/www/schleuder/raw-images/kitty.jpg';
+
+  	res.sendFile(filename);
+
+});
+
+/* 
 router.get('/', function(req, res, next) {
   
   var filename = '/var/www/schleuder/raw-images/kitty.jpg';
 
-fs.readFile(filename, function(err, buffer){
-  // check err
-  lwip.open(buffer, 'jpg', function(err, image){
-      // check 'err'. use 'image'.
-      // image.resize(...), etc.
-      image.toBuffer('jpeg', {quality:1}, function(err, buffer){
+  open(filename)
+  .then(rotate)
+  .then(resize)
+  .then(function(image){
+      image.toBuffer('jpeg', {quality:100}, function(err, buffer){
       	res.contentType('image/jpeg');
       	res.send(buffer);
-      });
+      });  	
   });
-});
-
-return;
-/*
-	lwip.open(filename, function(error, image){
-		if(error){
-			return;
-		}
-		console.log(image)
-// check err...
-  // manipulate image:
-  image.scale(1.2, function(err, image){
-  	image.writeFile('/var/www/schleuder/raw-images/kitty2.jpg', 'jpg', {}, function(){
-  		res.sendFile('/var/www/schleuder/raw-images/kitty2.jpg');
-  	});
-        
 
 });
 
-
-
-	});
-return;
 */
+router.get('/', function(request, response, next){
+    var imageUrl = '/var/www/schleuder/raw-images/moon.jpeg';
 
-  open(fileName)
+    open(imageUrl)
+    .then(rotate)
+  .then(resize)
   .then(function(image){
-		image.scale(0.5, function(error, image){
-		res.send(image);
-		
-	});
-
-
-  })
-  .then(function(image){
-  	
-})
-  ;
-
-
-  return;
-
-
-
-  // check err...
-  // manipulate image:
-  image.scale(1.2, function(err, image){
-  	image.writeFile('/var/www/schleuder/raw-images/kitty2.jpg', 'jpg', {}, function(){
-  		res.sendFile('/var/www/schleuder/raw-images/kitty2.jpg');
-  	});
-        
-
-});
-	
+      image.toBuffer('jpeg', {quality:100}, function(err, buffer){
+        response.contentType('image/jpeg');
+        response.send(buffer);
+      });   
+  });
 
 });
 
--- schleuder_modules/actions/open.js
+++ schleuder_modules/actions/open.js
@@ -1,8 +1,10 @@
 var lwip = require('lwip');
 var q = require('q');
 var fs = require('fs');
+var http = require('http');
+require('buffer');
 
-var open = function(filename){
+var openLocal = function(filename){
 	var deferred = q.defer();
 
 	fs.readFile(filename, function(error, buffer){
@@ -18,4 +20,83 @@ var open = function(filename){
 	return deferred.promise;
 };
 
-module.exports = open;
+var openRemote = function(filename){
+
+	var deferred = q.defer();	
+
+
+	fs.readFile(filename, function(error, fsBuffer){
+		if(error){
+			deferred.reject();
+		}
+
+
+		var options = {
+		  hostname: '3.f.ix.de',
+		  port: 80,
+		  path: '/scale/geometry/600/q75/imgs/18/1/4/5/3/3/9/9/16249197804_1806b101a2_k-89f4f4dd5091b029.jpeg',
+		  method: 'GET',
+		};
+
+		var req = http.request(options, function(res) {
+
+			var data = [];
+
+
+			var body;
+		  
+
+		  res.on('data', function (chunk) {
+		    body += chunk
+
+		    data.push(chunk)
+
+		  });
+		  res.on('end', function() {
+
+				var buffer = Buffer.concat(data);
+				console.log(buffer)
+
+		  		lwip.open(buffer, 'jpg', function(error, image){
+		  			console.log(error)
+		  			console.log(image)
+		  			deferred.resolve(image);	
+				});
+
+
+		  })
+		});
+
+		req.on('error', function(e) {
+			console.log(e);
+		  console.log('problem with request: ' + e.message);
+		});
+
+		req.end();
+	});
+
+
+	return deferred.promise;	
+
+	http.get(filename).on('response', function (response) {
+		res.setEncoding('binary');
+    	var body = '';
+    
+    	response.on('data', function (chunk) {
+        	body += chunk;
+    	});
+    	response.on('end', function () {
+			console.log(body)
+	  		lwip.open(body, 'jpg', function(error, image){
+	  			console.log(error)
+	  			console.log(image)
+	  			//deferred.resolve(image);	
+			});
+		});
+	});
+
+	return deferred.promise;	
+};
+
+
+module.exports = openRemote;
