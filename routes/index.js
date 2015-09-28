1f72c0dda447148c18ec6152287fb16db86ee8e7
samisdat <bastian.sackermann@gmail.com>
2015-09-28 19:27:13 +0000

    different routes for different images

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 var lwip = require('lwip');
 
-var open = require('../schleuder_modules/actions/open');
-var resize = require('../schleuder_modules/actions/resize');
-var scale = require('../schleuder_modules/actions/scale');
-var rotate = require('../schleuder_modules/actions/flip');
-var send = require('../schleuder_modules/actions/send');
+var schleuder = require('../schleuder_modules/schleuder');
 
 router.get('/local-image', function(req, res, next) {
   
@@ -17,37 +13,10 @@ router.get('/local-image', function(req, res, next) {
 
 });
 
-/* 
-router.get('/', function(req, res, next) {
-  
-  var filename = '/var/www/schleuder/raw-images/kitty.jpg';
+router.get('/foo', schleuder);
 
-  open(filename)
-  .then(rotate)
-  .then(resize)
-  .then(function(image){
-      image.toBuffer('jpeg', {quality:100}, function(err, buffer){
-      	res.contentType('image/jpeg');
-      	res.send(buffer);
-      });  	
-  });
+router.get('/foobar', schleuder);
 
-});
 
-*/
-router.get('/', function(request, response, next){
-    var imageUrl = '/var/www/schleuder/raw-images/moon.jpeg';
-
-    open(imageUrl)
-    .then(rotate)
-  .then(resize)
-  .then(function(image){
-      image.toBuffer('jpeg', {quality:100}, function(err, buffer){
-        response.contentType('image/jpeg');
-        response.send(buffer);
-      });   
-  });
-
-});
 
 module.exports = router;
