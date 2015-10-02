aac541bbc715417d7e12cb20ef5b7dec2d277b32
samisdat <bastian.sackermann@gmail.com>
2015-10-02 21:32:08 +0000

    test heatmap with threshold while rga differs a little

--- app/seamcarver.js
+++ app/seamcarver.js
@@ -1,19 +1,34 @@
+var lwip = require('lwip');
 
 var seamCarver = function(lwipImage){
 
     var width = lwipImage.width();
     var height = lwipImage.height();
 
+    var pixels = []
+
     var getWidth = function(){
-        return lwipImage.width();
+        return width;
     };
 
     var getHeight = function(){
-        return lwipImage.height();
+        return height;
     };
 
-    var getPixel = function(){
+    var setPixels = function(){
+        for (var x = 0, width = getWidth(); x < width;  x += 1) {
+            pixels[x] = [];
+            for (var y = 0, height = getHeight(); y < height;  y += 1) {
+                    pixels[x][y] = lwipImage.getPixel(x, y);
+            }
 
+        }
+    }
+    setPixels();
+
+    var getPixel = function(x, y){
+        //@TODO Validate params
+        return pixels[x][y];
     };
 
     var putPixel = function(x, y, color){
@@ -25,6 +40,35 @@ var seamCarver = function(lwipImage){
     };
 
     var initHeatMap = function(){
+        var b = function(x, y){
+            if (x < 0 || y < 0 || x >= getWidth() || y >= getHeight()) {
+                return 0;
+            }
+            var pixel = getPixel(x, y);
+            return (pixel.r + pixel.g + pixel.b);
+        }
+
+        var heatMap = [];
+        var max = 0;
+        for (var x = 0, width = getWidth(); x < width;  x += 1) {
+            heatMap[x] = [];
+            for (var y = 0; y < getHeight(); y++) {
+                var xenergy = b(x - 1, y - 1) + 2 * b(x - 1, y) + b(x - 1, y + 1) - b(x + 1, y - 1) - 2 * b(x + 1, y) - b(x + 1, y + 1)
+                var yenergy = b(x - 1, y - 1) + 2 * b(x, y - 1) + b(x + 1, y - 1) - b(x - 1, y + 1) - 2 * b(x, y + 1) - b(x + 1, y + 1)
+                heatMap[x][y] = Math.sqrt(xenergy * xenergy + yenergy * yenergy);
+                if(7 === 8 && 12 === x && 424 === y){
+                    console.log(b(x + 1, y + 1))
+                    console.log(getPixel(x + 1, y + 1));
+                }
+                max = (max > heatMap[x][y] ? max : heatMap[x][y]);
+            }
+        }
+        maxHeat = max;
+
+        return{
+            maxHeat: maxHeat,
+            heatMap: heatMap
+        }
 
     };
 
@@ -34,9 +78,9 @@ var seamCarver = function(lwipImage){
 
     var getHeatMap = function(){
 
-        var heatMap= initHeatMap();
+        var heatMap = initHeatMap();
 
-        lwip.create(image.width(), image.height(), {r:0, g:0, b:0}, function(err, blankImage){
+        lwip.create(getWidth(), getHeight(), {r:0, g:0, b:0}, function(err, blankImage){
 
             var batch = blankImage.batch();
 
@@ -67,6 +111,7 @@ var seamCarver = function(lwipImage){
     return {
         getWidth: getWidth,
         getHeight: getHeight,
+        initHeatMap:initHeatMap,
         getHeatMap: getHeatMap,
         getSeams:getSeams,
         resize:resize
--- test/seamcarver.js
+++ test/seamcarver.js
@@ -3,6 +3,7 @@
  * https://github.com/axemclion/seamcarving
  */
 
+var fs = require ('fs');
 var lwip = require('lwip');
 var expect    = require("chai").expect;
 var seamCarver = require("../app/seamcarver");
@@ -36,7 +37,43 @@ describe("SeamCarver", function() {
 	});	
 
 	describe("Get Heat Map", function() {
-    	it("generate a heatmap of a given image", function() {
+
+		this.timeout(6000);
+
+    	it("generate a heatmap of a given image", function(done) {
+
+			var seamCarving = seamCarver(lwipImage);
+			var heatmap = seamCarving.initHeatMap();
+
+			var workerHeatMapFile = __dirname + '/data/heatmap.json';
+
+			var workerHeatMap = fs.readFileSync(workerHeatMapFile, 'utf8');
+			workerHeatMap = JSON.parse(workerHeatMap);
+
+			expect(heatmap.heatMap.length).to.equal(workerHeatMap.length);
+
+			var threshold = 2;
+
+			for(var i = 0, x = heatmap.heatMap.length; i < x; i += 1){
+				for(var j = 0, y = heatmap.heatMap[i].length; j < y; j += 1){
+					var nodejsHeatMapItem = heatmap.heatMap[i][j];
+					var workerHeatMapItem = workerHeatMap[i][j];
+
+					var isInThreshold = false;
+					if(nodejsHeatMapItem === workerHeatMapItem){
+						isInThreshold = true;
+					}
+					if(nodejsHeatMapItem < workerHeatMapItem && (nodejsHeatMapItem + threshold) >= workerHeatMapItem){
+						isInThreshold = true;	
+					}
+					if(nodejsHeatMapItem > workerHeatMapItem && (nodejsHeatMapItem - threshold) <= workerHeatMapItem){
+						isInThreshold = true;	
+					}
+					expect(isInThreshold).to.equal(true, nodejsHeatMapItem + ' ' + workerHeatMapItem);
+				}
+			}
+
+			done();
 		});
 	});
 
