febd8e6840a440541c2305469d7e375c1357d021
samisdat <bastian.sackermann@gmail.com>
2015-10-02 19:47:24 +0000

    take methods and their signature from https://github.com/axemclion/seamcarving/blob/gh-pages/SeamCarver.js

--- app/seamcarver.js
+++ app/seamcarver.js
@@ -1,1 +1,45 @@
-seamcarver.js
+
+var seamCarver = function(lwipImage){
+
+    var getPixel = function(){
+
+    };
+
+    var putPixel = function(x, y, color){
+
+    };
+
+    var copyImage = function(){
+
+    };
+
+    var initHeatMap = function(){
+
+    };
+
+    var initSeams = function(){
+
+    };
+
+    var getHeatMap = function(){
+
+    };
+
+    var getSeams = function(){
+
+    };
+
+    var resize = function(dim){
+        
+    };
+
+    return {
+        getHeatMap: getHeatMap,
+        getSeams:getSeams,
+        resize:resize
+    };
+};
+
+
+module.exports = seamCarver;
+
