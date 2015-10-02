4473683ff3860a6c1fb70e22f76f3ec8dccf3810
samisdat <bastian.sackermann@gmail.com>
2015-10-02 20:11:20 +0000

    test should kown image's dimensions

--- app/seamcarver.js
+++ app/seamcarver.js
@@ -1,6 +1,17 @@
 
 var seamCarver = function(lwipImage){
 
+    var width = lwipImage.width();
+    var height = lwipImage.height();
+
+    var getWidth = function(){
+        return lwipImage.width();
+    };
+
+    var getHeight = function(){
+        return lwipImage.height();
+    };
+
     var getPixel = function(){
 
     };
@@ -23,6 +34,26 @@ var seamCarver = function(lwipImage){
 
     var getHeatMap = function(){
 
+        var heatMap= initHeatMap();
+
+        lwip.create(image.width(), image.height(), {r:0, g:0, b:0}, function(err, blankImage){
+
+            var batch = blankImage.batch();
+
+            for (var x = 0; x < blankImage.width(); x++) {
+                for (var y = 0; y < blankImage.height(); y++) {
+                    var color = parseInt(heatMap[x][y] / maxHeat * 255, 10);
+                    
+                    batch.setPixel(x, y, {r:color, g:color, b:color});
+    
+                }
+            }
+                
+            batch.writeFile('/var/www/schleuder/public/heatmap.jpg', 'jpg', {quality:100}, function(){});
+
+        });
+
+
     };
 
     var getSeams = function(){
@@ -30,10 +61,12 @@ var seamCarver = function(lwipImage){
     };
 
     var resize = function(dim){
-        
+
     };
 
     return {
+        getWidth: getWidth,
+        getHeight: getHeight,
         getHeatMap: getHeatMap,
         getSeams:getSeams,
         resize:resize
--- test/seamcarver.js
+++ test/seamcarver.js
@@ -1,1 +1,43 @@
-seamcarver.js
+/*
+ * Testdriven reverse implementing
+ * https://github.com/axemclion/seamcarving
+ */
+
+var lwip = require('lwip');
+var expect    = require("chai").expect;
+var seamCarver = require("../app/seamcarver");
+
+//getHeatMap: getHeatMap,
+//getSeams:getSeams,
+//resize:resize
+
+describe("SeamCarver", function() {
+
+	var lwipImage;
+
+	before(function(done) {
+		lwip.open('/var/www/schleuder/raw-images/ballon.jpg', function(error, _lwipImage){
+			if(error){
+				return done(error);
+			}
+			lwipImage = _lwipImage;
+			done();
+		});
+	});
+
+	it('should know image\'s dimensions', function(done){
+
+		var seamCarving = seamCarver(lwipImage);
+
+		expect(seamCarving.getWidth()).to.equal(500);
+		expect(seamCarving.getHeight()).to.equal(500);
+
+		done();
+	});	
+
+	describe("Get Heat Map", function() {
+    	it("generate a heatmap of a given image", function() {
+		});
+	});
+
+});
