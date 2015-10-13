1f438ea278a7abbd7b2239c8e86ec3d0812b1e17
samisdat <bastian.sackermann@gmail.com>
2015-10-13 20:21:08 +0000

    liquidpixel check and mark as deleted

---
+++ app/liquid-scaling/liquid-pixel.js
@@ -0,0 +1,31 @@
+var Pixel = require('./pixel');
+
+var LiquidPixel = function(x, y, r, g, b, a) {
+    Pixel.call(this, x, y, r, g, b, a);
+    
+    this.heat = undefined;
+    
+    this.deleted = false;
+};
+
+LiquidPixel.prototype = Object.create(Pixel.prototype);
+LiquidPixel.prototype.constructor = LiquidPixel;
+
+
+LiquidPixel.prototype.getHeat = function(){
+    return this.heat;  
+};
+
+LiquidPixel.prototype.setHeat = function(heat){
+    this.heat = heat;  
+};
+
+LiquidPixel.prototype.isDeleted = function(){
+    return (this.deleted === true);
+};
+	
+LiquidPixel.prototype.markAsDeleted = function(){
+    this.deleted = true;
+};
+
+module.exports = LiquidPixel;
